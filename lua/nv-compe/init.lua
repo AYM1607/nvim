69d18edccb5dd39c8904715e96bd13ab995455ea
AYM1607 <u.g.a.mariano@gmail.com>
2021-06-30 21:54:30 -0500

    Nv-compe now always preselects, and confirm with Tab. Do not use buffer as source.

--- lua/nv-compe/init.lua
+++ lua/nv-compe/init.lua
@@ -1,22 +1,24 @@
 vim.o.completeopt = "menuone,noselect"
+-- Avoid having a "Pattern not found" message
+vim.cmd("set shortmess+=c")
 
 require'compe'.setup {
   enabled = true;
   autocomplete = true;
   debug = false;
   min_length = 1;
-  preselect = 'enable';
+  preselect = 'always';
   throttle_time = 80;
   source_timeout = 200;
   incomplete_delay = 400;
   max_abbr_width = 100;
   max_kind_width = 100;
   max_menu_width = 100;
-  documentation = false;
+  documentation = true;
 
   source = {
     path = true;
-    buffer = true;
+    buffer = false;
     calc = true;
     vsnip = true;
     nvim_lsp = true;
@@ -31,45 +33,6 @@ local t = function(str)
   return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
-end
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
---_G.tab_complete_only = function()
---  if vim.fn.pumvisible() == 1 then
---    return t "<C-n>"
---  else
---    return t "<Tab>"
---  end
---end
---_G.tab_complete = function()
---  if vim.fn.pumvisible() == 1 then
---    return t "<C-n>"
---  elseif vim.fn.call("vsnip#available", {1}) == 1 then
---    return t "<Plug>(vsnip-expand-or-jump)"
---  elseif check_back_space() then
---    return t "<Tab>"
---  else
---    return vim.fn['compe#complete']()
---  end
---end
---_G.s_tab_complete = function()
---  if vim.fn.pumvisible() == 1 then
---    return t "<C-p>"
---  elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
---    return t "<Plug>(vsnip-jump-prev)"
---  else
---    return t "<S-Tab>"
---  end
---end
 _G.comp_jump_if_avail = function()
   if vim.fn.pumvisible() == 1 then
     return t "<C-n>"
@@ -87,10 +50,21 @@ _G.tab_complete = function()
     return t "<Tab>"
   end
 end
+_G.enter_complete = function() 
+  if vim.fn.pumvisible() == 1 then
+    return vim.fn['compe#confirm']('')
+  else
+    return t "<CR>"
+  end
+end
 
 vim.api.nvim_set_keymap("i", "<C-j>", "v:lua.comp_jump_if_avail()", {expr = true})
 vim.api.nvim_set_keymap("s", "<C-j>", "v:lua.comp_jump_if_avail()", {expr = true})
+-- The next 4 funtcions don't work if compe doesn't automatically select the first option.
 vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<CR>", "v:lua.enter_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<CR>", "v:lua.enter_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<C-k>", "v:lua.comp_jump_prev_if_avail()", {expr = true})
 vim.api.nvim_set_keymap("s", "<C-k>", "v:lua.comp_jump_prev_if_avail()", {expr = true})
+
