70fc7c6cbcd40c90016ddb246343040efb2636ec
AYM1607 <u.g.a.mariano@gmail.com>
2021-07-14 14:48:20 -0500

    Add autoformatting to python files.

--- lua/lsp/python-ls.lua
+++ lua/lsp/python-ls.lua
@@ -2,3 +2,6 @@ require'lspconfig'.pyright.setup{
     cmd = {DATA_PATH .. "/lspinstall/python/node_modules/.bin/pyright-langserver", "--stdio"},
 }
 
+-- Autoformat on save.
+vim.cmd("autocmd BufWritePre *.py call Black()")
+
